daaac4d82da63d40f3f4f452b8c9fe35a0759f48
aafanasyevaa <afanasyevaa.anastasiia@gmail.com>
2022-02-19 23:11:59 +0300

    added testdata

---
+++ src/test/java/aafanasyevaa/api/ApiTestData.java
@@ -0,0 +1,11 @@
+package aafanasyevaa.api;
+
+public class ApiTestData {
+
+    public final String email = "janet.weaver@reqres.in";
+    public final String loginData = "{\"email\": \"eve.holt@reqres.in\",\"password\": \"pistol\" }";
+    public final String responseData =  "{ \"id\": 6, \"email\": \"tracey.ramos@reqres.in\", \"first_name\": \"Tracey\", \"last_name\": \"Ramos\", \"avatar\": \"https://reqres.in/img/faces/6-image.jpg\" }";
+    public final String updateData = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+    public final String expectedToken = "QpwL5tke4Pnpja7X4";
+    public final String expectedResponse = "morpheus";
+}
